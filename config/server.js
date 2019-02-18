bdbfa9aa7738e1e4d2559cbdeea5522c72d97633
Tyrese Williams <tyresewilliams@Tyreses-MacBook-Pro.local>
2019-02-17 20:02:33 -0500

    .

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 'd102609ed8313402ce16c9a7103fa6938e8b0e6b86db050cf1ea31e9e08ce647',
+    SECRET: process.env.SECRET || '0a957a256e49f530241bec789f987cd4758404f50eada5c5bd32e7b95f942a0a',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
